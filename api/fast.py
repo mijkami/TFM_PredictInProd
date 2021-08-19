eff240539530f2a91b5b47f7e6401ef9486002f1
Dimitri GAILLOT <dimitri.gaillot@gmail.com>
2021-08-19 15:43:21 +0200

    Initial Push

---
+++ api/fast.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+from datetime import datetime
+import pytz
+import joblib
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+@app.get("/")
+def index():
+    return {"greeting": "Hello world"}
+
+@app.get("/predict")
+def predict(pickup_datetime, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, passenger_count):
+    params = {
+                "key" : ["2013-07-06 17:18:00.000000119"],
+                "pickup_datetime": [pickup_datetime],
+                "pickup_longitude": [pickup_longitude],
+                "pickup_latitude": [pickup_latitude],
+                "dropoff_longitude": [dropoff_longitude],
+                "dropoff_latitude": [dropoff_latitude],
+                "passenger_count": [passenger_count]
+                }
+    X_pred = pd.DataFrame.from_dict(params)
+    pickup_datetime = datetime.strptime(X_pred["pickup_datetime"][0], "%Y-%m-%d %H:%M:%S")
+    eastern = pytz.timezone("US/Eastern")
+    localized_pickup_datetime = eastern.localize(pickup_datetime, is_dst=None)
+    utc_pickup_datetime = localized_pickup_datetime.astimezone(pytz.utc)
+    formatted_pickup_datetime = utc_pickup_datetime.strftime("%Y-%m-%d %H:%M:%S UTC")
+    X_pred["pickup_datetime"] = formatted_pickup_datetime
+    model = joblib.load('model.joblib')
+    y_pred = model.predict(X_pred)[0]
+    return {"prediction": y_pred}
